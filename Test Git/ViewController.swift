5349e26915235438a1e86a75c4a3a55aba8d7d60
ciro.millan@grupohasten.com <ciro.millan@grupohasten.com>
2022-02-22 14:10:51 +0100

    Init commit

---
+++ Test Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Git
+//
+//  Created by Hasten on 22/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
